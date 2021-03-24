9803dd6ac7fde0ee451c4193825b592f9bddeaec
Jon Hatfield <johatfie@gmail.com>
2021-03-24 02:34:48 -0400

    Add hibernate UserType for Dollars

---
+++ src/main/java/com/example/widgets_are_us/productsservice/utils/DollarsType.java
@@ -0,0 +1,68 @@
+package com.example.widgets_are_us.productsservice.utils;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.usertype.UserType;
+
+import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DollarsType implements UserType {
+
+    @Override
+    public int[] sqlTypes() {
+        return new int[0];
+    }
+
+    @Override
+    public Class<Dollars> returnedClass() {
+        return Dollars.class;
+    }
+
+    @Override
+    public boolean equals(Object o, Object o1) throws HibernateException {
+        return o.equals(o1);
+    }
+
+    @Override
+    public int hashCode(Object o) throws HibernateException {
+        return o.hashCode();
+    }
+
+    @Override
+    public Object nullSafeGet(ResultSet resultSet, String[] strings, SharedSessionContractImplementor sharedSessionContractImplementor, Object o) throws HibernateException, SQLException {
+        return null;
+    }
+
+    @Override
+    public void nullSafeSet(PreparedStatement preparedStatement, Object o, int i, SharedSessionContractImplementor sharedSessionContractImplementor) throws HibernateException, SQLException {
+
+    }
+
+    @Override
+    public Object deepCopy(Object o) throws HibernateException {
+        return o;
+    }
+
+    @Override
+    public boolean isMutable() {
+        return false;
+    }
+
+    @Override
+    public Serializable disassemble(Object o) throws HibernateException {
+        return (Serializable) o;
+    }
+
+    @Override
+    public Object assemble(Serializable serializable, Object o) throws HibernateException {
+        return serializable;
+    }
+
+    @Override
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
+}
